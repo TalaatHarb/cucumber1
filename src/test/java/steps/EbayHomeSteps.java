a92778de88087fd5bf547583ec2dd74f57b71fca
mharb <mohamed.harb@cegedim.com>
2024-08-20 12:46:52 +0300

    added first seleinum actions

--- src/test/java/steps/EbayHomeSteps.java
+++ src/test/java/steps/EbayHomeSteps.java
@@ -1,5 +1,11 @@
 package steps;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -8,18 +14,30 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class EbayHomeSteps {
 
+	private static final String HOME_PAGE_URL = "https://www.ebay.com/";
+	private static final String ADVANCED_SEARCH_PAGE_URL = "https://www.ebay.com/sch/ebayadvsearch";
+	
+	private WebDriver webDriver;
+
 	@Given("I am on Ebay Home page")
 	public void iAmOnEbayHomePage() {
+		webDriver = new ChromeDriver();
+		
+		webDriver.get(HOME_PAGE_URL);
+		
 		log.info("I am on Ebay Home page");
 	}
 
 	@When("I click on Advanced Link")
 	public void iClickOnAdvancedLink() {
+		webDriver.findElement(By.linkText("Advanced")).click();
 		log.info("I click on Advanced Link");
 	}
 
 	@Then("I naviagate to the Advanced Search page")
 	public void iNaviagateToTheAdvancedSearchPage() {
+		String currentURL = webDriver.getCurrentUrl();
+		assertEquals(ADVANCED_SEARCH_PAGE_URL, currentURL);
 		log.info("I naviagate to the Advanced Search page");
 	}
 }
