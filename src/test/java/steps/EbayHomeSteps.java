a84b1fda527d477d4c4e9c48bb72eee3d2764e22
mharb <mohamed.harb@cegedim.com>
2024-08-20 15:01:48 +0300

    some refactor and clean up

--- src/test/java/steps/EbayHomeSteps.java
+++ src/test/java/steps/EbayHomeSteps.java
@@ -2,57 +2,29 @@ package steps;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.time.Duration;
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import lombok.extern.slf4j.Slf4j;
+import utils.WebDriverUtils;
 
 @Slf4j
 public class EbayHomeSteps {
 
-	private static final long IMPLICIT_WAIT = 60L;
 	private static final String HOME_PAGE_URL = "https://www.ebay.com/";
 	private static final String ADVANCED_SEARCH_PAGE_URL = "https://www.ebay.com/sch/ebayadvsearch";
-	private static final List<String> CHROME_ARGUMENTS = List.of("ignore-certificate-errors", "disable-extensions",
-			"window-size=1920,1080", "no-sandbox", "disable-dev-shm-usage");
-
-	private static final List<String> HEADLESS_CHROME_ARGUMENTS = List.of("headless", "disable-gpu",
-			"remote-debugging-port=9222");
-
 	private WebDriver webDriver;
 
 	@Given("I am on Ebay Home page")
 	public void iAmOnEbayHomePage() {
-		createWebDriver();
+		webDriver = WebDriverUtils.createWebDriver();
 
 		webDriver.get(HOME_PAGE_URL);
 
 		log.info("I am on Ebay Home page");
-	}
-
-	private void createWebDriver() {
-		final ChromeDriverService service = (new ChromeDriverService.Builder()).build();
-		final ChromeOptions options = new ChromeOptions();
-		options.addArguments(CHROME_ARGUMENTS);
-
-		String os = System.getProperty("os.name");
-		log.info("OS: {}", os);
-		if (!os.contains("Windows")) {
-			options.addArguments(HEADLESS_CHROME_ARGUMENTS);
-		}
-
-		webDriver = new ChromeDriver(service, options);
-		webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICIT_WAIT));
-		webDriver.manage().window().maximize();
 	}
 
 	@When("I click on Advanced Link")
@@ -65,7 +37,7 @@ public class EbayHomeSteps {
 	public void iNaviagateToTheAdvancedSearchPage() {
 		String currentURL = webDriver.getCurrentUrl();
 		assertEquals(ADVANCED_SEARCH_PAGE_URL, currentURL);
-		webDriver.close();
+		webDriver.quit();
 		log.info("I naviagate to the Advanced Search page");
 	}
 }
---
+++ src/test/java/utils/WebDriverUtils.java
@@ -0,0 +1,39 @@
+package utils;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class WebDriverUtils {
+
+	public static final List<String> HEADLESS_CHROME_ARGUMENTS = List.of("headless", "disable-gpu",
+	"remote-debugging-port=9222");
+	public static final long IMPLICIT_WAIT = 60L;
+	public static final List<String> CHROME_ARGUMENTS = List.of("ignore-certificate-errors", "disable-extensions",
+	"window-size=1920,1080", "no-sandbox", "disable-dev-shm-usage");
+	public static final WebDriver createWebDriver() {
+		final ChromeDriverService service = (new ChromeDriverService.Builder()).build();
+		final ChromeOptions options = new ChromeOptions();
+		options.addArguments(CHROME_ARGUMENTS);
+	
+		String os = System.getProperty("os.name");
+		log.debug("OS: {}", os);
+		if (!os.contains("Windows")) {
+			options.addArguments(HEADLESS_CHROME_ARGUMENTS);
+		}
+	
+		ChromeDriver chromeDriver = new ChromeDriver(service, options);
+		chromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICIT_WAIT));
+		chromeDriver.manage().window().maximize();
+		
+		return chromeDriver;
+	}
+
+}
