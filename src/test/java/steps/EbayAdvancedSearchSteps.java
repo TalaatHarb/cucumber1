258be07f1e26ce4db1236ecd81379c8743184b1e
mharb <mohamed.harb@cegedim.com>
2024-08-21 13:31:03 +0300

    added advanced search scenario with data table feature

--- src/test/java/steps/EbayAdvancedSearchSteps.java
+++ src/test/java/steps/EbayAdvancedSearchSteps.java
@@ -2,14 +2,16 @@ package steps;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.When;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class EbayAdvancedSearchSteps {
 	private WebDriver webDriver;
-	
+
 	public EbayAdvancedSearchSteps(CommonSteps commonSteps) {
 		webDriver = commonSteps.getWebDriver();
 	}
@@ -19,5 +21,24 @@ public class EbayAdvancedSearchSteps {
 		var ebayLogo = webDriver.findElement(By.xpath("//*[@id='gh-logo']"));
 		ebayLogo.click();
 		log.info("I click on Ebay logo");
+	}
+
+	@When("I do advanced search")
+	public void iDoAdvancedSearch(DataTable dataTable) {
+		int size = dataTable.height();
+		for (int i = 1; i < size; i++) {
+			webDriver.findElement(By.xpath("//*[@id=\"_nkw\"]")).sendKeys(dataTable.cell(i, 0));
+			webDriver.findElement(By.xpath("//*[@id=\"_ex_kw\"]")).sendKeys(dataTable.cell(i, 1));
+			webDriver.findElement(By.cssSelector("[aria-label=\"Enter minimum price range value, $\"]"))
+					.sendKeys(dataTable.cell(i, 2));
+			webDriver.findElement(By.cssSelector("[aria-label=\"Enter maximum price range value, $\"]"))
+					.sendKeys(dataTable.cell(i, 3));
+
+			var searchForm = webDriver.findElements(By.tagName("form")).stream()
+					.filter(f -> !"gh-f".equals(f.getAttribute("id"))).findFirst();
+			if (searchForm.isPresent()) {
+				searchForm.get().findElement(By.tagName("button")).click();
+			}
+		}
 	}
 }
--- src/test/resources/features/EbayAdvancedSearch.feature
+++ src/test/resources/features/EbayAdvancedSearch.feature
@@ -6,3 +6,11 @@ Feature: Advanced search page features
     Given I am on Ebay 'Advanced search' page
     When I click on Ebay logo
     Then I naviagate to the 'Home' page
+
+  @Search
+  Scenario: Advanced search
+    Given I am on Ebay 'Advanced search' page
+    When I do advanced search
+      | keyword   | exclude     | minPrice | maxPrice |
+      | IPhone 14 | refurbished |      300 |      900 |
+    Then I confirm at least 1000 results
