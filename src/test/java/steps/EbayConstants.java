a9fb0178c240ee1b1ce0c6e6188b09402c881db8
mharb <mohamed.harb@cegedim.com>
2024-08-27 11:38:19 +0300

    extracted url into a variable to be able to override

--- src/test/java/steps/EbayConstants.java
+++ src/test/java/steps/EbayConstants.java
@@ -6,9 +6,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class EbayConstants {
 
-	public static final String ADVANCED_SEARCH_PAGE_URL = "https://www.ebay.com/sch/ebayadvsearch";
-	public static final String HOME_PAGE_URL = "https://www.ebay.com/";
-	public static final String AUTO_PAGE = "https://www.ebay.com/b/Auto-Parts";
-	public static final String ELECTRONICS_PAGE = "https://www.ebay.com/b/Electronics";
-	
+	public static final String DEFAULT_URL = "https://www.ebay.com";
+	public static final String URL_PROPERTY = "site.url";
+	public static final String MAIN_URL = System.getProperty(URL_PROPERTY, DEFAULT_URL);
+	public static final String ADVANCED_SEARCH_PAGE_URL = MAIN_URL + "/sch/ebayadvsearch";
+	public static final String HOME_PAGE_URL = MAIN_URL + "/";
+	public static final String AUTO_PAGE = MAIN_URL + "/b/Auto-Parts";
+	public static final String ELECTRONICS_PAGE = MAIN_URL + "/b/Electronics";
+
 }
